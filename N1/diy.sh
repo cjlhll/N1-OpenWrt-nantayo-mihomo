adcb6986dee9c9252d4e8c4b3bb0090d6349dd17
AJ <983097239@qq.com>
2024-09-01 12:18:23 +0800

    Update diy.sh

--- N1/diy.sh
+++ N1/diy.sh
@@ -3,9 +3,23 @@
 # Default IP
 sed -i 's/192.168.1.1/192.168.5.2/g' package/base-files/files/bin/config_generate
 
-# Remove packages
-rm -rf feeds/luci/applications/luci-app-passwall
+#修改密码
+sed -i 's/^root:.*:/root:$1$KVHNuqbv$4X2BPbtsXn2AApknHIn38.:0:0:99999:7:::/g' package/base-files/files/etc/shadow
 
-# Add packages
-git clone https://github.com/nantayo/My-Pkg package/my-pkg
-git clone --depth=1 https://github.com/ophub/luci-app-amlogic package/amlogic
+# => alist
+rm -rf feeds/packages/lang/golang
+git clone https://github.com/sbwml/packages_lang_golang -b 22.x feeds/packages/lang/golang
+git clone https://github.com/sbwml/luci-app-alist package/alist
+
+# => 阿里ddns
+git clone https://github.com/chenhw2/luci-app-aliddns.git package/luci-app-aliddns
+
+# => OpenClash
+git clone --depth=1 https://github.com/vernesong/OpenClash.git package/OpenClash
+
+# => 删除默认argon主题
+rm -rf feeds/luci/themes/luci-theme-argon
+
+#主题
+git clone https://github.com/jerrykuku/luci-theme-argon package/luci-theme-argon
+sed -i 's/luci-theme-bootstrap/luci-theme-argon/g' ./feeds/luci/collections/luci/Makefile
