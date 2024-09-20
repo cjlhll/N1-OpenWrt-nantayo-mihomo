2e8f8302e9613c7682366e8b15096650c92bd944
AJ <983097239@qq.com>
2024-09-20 22:25:05 +0800

    Update diy.sh

--- N1/diy.sh
+++ N1/diy.sh
@@ -14,7 +14,6 @@ git clone https://github.com/sbwml/luci-app-alist package/alist
 # => 阿里ddns
 git clone https://github.com/chenhw2/luci-app-aliddns.git package/luci-app-aliddns
 
-# => passwall2
-git clone https://github.com/xiaorouji/openwrt-passwall-packages.git -b main package/passwall_package
-git clone https://github.com/xiaorouji/openwrt-passwall2.git package/passwall2
+# => mihomo
+git clone https://github.com/morytyann/OpenWrt-mihomo package/mihomo
 
