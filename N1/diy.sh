288b664bf5a611b2e29a25aeb8f9eac86f9d9c6c
AJ <983097239@qq.com>
2024-12-25 16:22:42 +0800

    Update diy.sh

--- N1/diy.sh
+++ N1/diy.sh
@@ -6,6 +6,9 @@ sed -i 's/192.168.1.1/192.168.5.2/g' package/base-files/files/bin/config_generat
 #修改密码
 sed -i 's/^root:.*:/root:$1$KVHNuqbv$4X2BPbtsXn2AApknHIn38.:0:0:99999:7:::/g' package/base-files/files/etc/shadow
 
+# => dockerman
+git clone https://github.com/lisaac/luci-app-dockerman.git package/luci-app-dockerman
+
 # => alist
 rm -rf feeds/packages/lang/golang
 git clone https://github.com/sbwml/packages_lang_golang -b 22.x feeds/packages/lang/golang
