ac5b8419190d63ac8748eb46c74469dac8daf2a0
AJ <983097239@qq.com>
2024-12-26 13:32:54 +0800

    删除dockerman

--- N1/diy.sh
+++ N1/diy.sh
@@ -7,15 +7,12 @@ sed -i 's/192.168.1.1/192.168.5.2/g' package/base-files/files/bin/config_generat
 sed -i 's/^root:.*:/root:$1$KVHNuqbv$4X2BPbtsXn2AApknHIn38.:0:0:99999:7:::/g' package/base-files/files/etc/shadow
 
 # => alist
-rm -rf feeds/packages/lang/golang
-git clone https://github.com/sbwml/packages_lang_golang -b 22.x feeds/packages/lang/golang
+#rm -rf feeds/packages/lang/golang
+#git clone https://github.com/sbwml/packages_lang_golang -b 22.x feeds/packages/lang/golang
 git clone https://github.com/sbwml/luci-app-alist package/alist
 
 # => 阿里ddns
 git clone https://github.com/chenhw2/luci-app-aliddns.git package/luci-app-aliddns
-
-# => dockerman
-git clone https://github.com/lisaac/luci-app-dockerman.git package/luci-app-dockerman
 
 # => mihomo
 git clone https://github.com/morytyann/OpenWrt-mihomo package/mihomo
