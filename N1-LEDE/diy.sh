7a54209b833e2c29e6df71f412d1e2c795058cef
AJ <983097239@qq.com>
2024-12-26 19:06:13 +0800

    添加LEAN版本

---
+++ N1-LEDE/diy.sh
@@ -0,0 +1,43 @@
+#!/bin/bash
+# Git稀疏克隆，只克隆指定目录到本地
+function git_sparse_clone() {
+  branch="$1" repourl="$2" && shift 2
+  git clone --depth=1 -b $branch --single-branch --filter=blob:none --sparse $repourl
+  repodir=$(echo $repourl | awk -F '/' '{print $(NF)}')
+  cd $repodir && git sparse-checkout set $@
+  mv -f $@ ../package
+  cd .. && rm -rf $repodir
+}
+
+#添加自定义的软件包源
+#git_sparse_clone main https://github.com/kiddin9/kwrt-packages ddns-go
+#git_sparse_clone main https://github.com/kiddin9/kwrt-packages luci-app-ddns-go
+
+# Remove packages
+#删除lean库中的插件，使用自定义源中的包。
+rm -rf feeds/luci/themes/luci-theme-argon
+rm -rf feeds/luci/applications/luci-app-argon-config
+rm -rf feeds/packages/net/mosdns
+rm -rf feeds/luci/applications/luci-app-mosdns
+#rm -rf feeds/luci/themes/luci-theme-design
+#rm -rf feeds/luci/applications/luci-app-design-config
+
+# Default IP
+sed -i 's/192.168.1.1/192.168.5.2/g' package/base-files/files/bin/config_generate
+
+#修改密码
+sed -i 's/^root:.*:/root:$1$KVHNuqbv$4X2BPbtsXn2AApknHIn38.:0:0:99999:7:::/g' package/base-files/files/etc/shadow
+
+# => alist
+rm -rf feeds/packages/lang/golang
+git clone https://github.com/sbwml/packages_lang_golang -b 22.x feeds/packages/lang/golang
+git clone https://github.com/sbwml/luci-app-alist package/alist
+
+# => 阿里ddns
+git clone https://github.com/chenhw2/luci-app-aliddns.git package/luci-app-aliddns
+
+# => mihomo
+git clone https://github.com/morytyann/OpenWrt-mihomo package/mihomo
+
+#修改默认时间格式
+sed -i 's/os.date()/os.date("%Y-%m-%d %H:%M:%S %A")/g' $(find ./package/*/autocore/files/ -type f -name "index.htm")
