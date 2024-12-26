82845420b303aa0f96a5d4c10355c0115cbc5804
AJ <983097239@qq.com>
2024-12-26 19:00:16 +0800

    添加openwrt官方版本

---
+++ N1_Official/diy.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+# Default IP
+sed -i 's/192.168.1.1/192.168.5.2/g' package/base-files/files/bin/config_generate
+
+#修改密码
+sed -i 's/^root:.*:/root:$1$KVHNuqbv$4X2BPbtsXn2AApknHIn38.:0:0:99999:7:::/g' package/base-files/files/etc/shadow
+
+# => alist
+#rm -rf feeds/packages/lang/golang
+#git clone https://github.com/sbwml/packages_lang_golang -b 22.x feeds/packages/lang/golang
+git clone https://github.com/sbwml/luci-app-alist package/alist
+
+# => 阿里ddns
+git clone https://github.com/chenhw2/luci-app-aliddns.git package/luci-app-aliddns
+
+# => mihomo
+git clone https://github.com/morytyann/OpenWrt-mihomo package/mihomo
