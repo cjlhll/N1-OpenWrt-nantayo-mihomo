df15405cef1e72f3d7994c3efccf6d6d06905a92
AJ <983097239@qq.com>
2024-12-30 15:33:03 +0800

    Update diy.sh

--- N1/diy.sh
+++ N1/diy.sh
@@ -8,8 +8,10 @@ sed -i 's/^root:.*:/root:$1$KVHNuqbv$4X2BPbtsXn2AApknHIn38.:0:0:99999:7:::/g' pa
 
 # => alist
 rm -rf feeds/packages/lang/golang
+rm -rf package/feeds/alist
 git clone https://github.com/sbwml/packages_lang_golang -b 22.x feeds/packages/lang/golang
-git clone https://github.com/sbwml/luci-app-alist package/alist
+git clone https://github.com/AlistGo/alist package/alist
+git clone https://github.com/sbwml/luci-app-alist package/luci-app-alist
 
 # => 阿里ddns
 git clone https://github.com/chenhw2/luci-app-aliddns.git package/luci-app-aliddns
